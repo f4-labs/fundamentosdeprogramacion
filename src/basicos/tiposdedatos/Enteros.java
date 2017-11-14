f8d294e67dc8907a79a1fb594ac551457e723f90
Jorge Martinez <faith92.koko@gmail.com>
2017-11-14 17:52:09 -0600

    agregar un ejemplo de tipos de datos primitivo enteros y con punto decimal

---
+++ src/basicos/tiposdedatos/Enteros.java
@@ -0,0 +1,35 @@
+package basicos.tiposdedatos;
+
+/**
+ * 
+ * @author Jorge
+ * 
+ * Las siguientes declaraciones se utilizan para declarar variables de tipo
+ * entero.
+ * 
+ * EJEMPLO:
+ * 
+ * 2, 7, 34, 90, 45
+ *
+ */
+
+public class Enteros { /* clase principal */
+
+	public static void main(String[] args) { /* metodo principal */
+		
+		/* BYTE . Ocupa 8 bits o 1 Bytes */
+		byte  numeroByte = 0;
+		
+		/* short . Ocupa 16 bits o 2 Bytes */
+		short numeroShort = 0;
+		
+		/* int . Ocupa 32 bits o 4 Bytes */
+		int numeroInt = 0;
+		
+		/* long . Ocupa 64 bits o 8 Bytes */
+		long numeroLong = 0;
+		
+		
+	}
+	
+}
---
+++ src/basicos/tiposdedatos/NumerosConPuntoDecimal.java
@@ -0,0 +1,28 @@
+package basicos.tiposdedatos;
+
+/**
+ * 
+ * @author Jorge
+ *
+ * Las siguientes declaraciones se utilizan para declarar variables de tipo
+ * flotante.
+ * 
+ * EJEMPLO:
+ * 
+ * 2.43, 7.5345345, 34.23435, 90.5467, 45.20
+ * 
+ */
+
+public class NumerosConPuntoDecimal { /* clase principal */
+
+	public static void main(String[] args) { /* metodo principal */
+		
+		/* float . Ocupa 32 bits o 4 Bytes */
+		float numeroFlotante = 0;
+		
+		/* double . Ocupa 64 bits o 8 Bytes */
+		double numeroDouble = 0;
+		
+	}
+	
+}
