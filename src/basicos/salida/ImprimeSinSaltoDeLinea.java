a3e9d1c5267884648aa3fc21b83930cdbd306378
Jorge Martinez <faith92.koko@gmail.com>
2017-11-14 14:58:53 -0600

    agregar un ejemplo de como imprimir sin saltos de linea

---
+++ src/basicos/salida/ImprimeSinSaltoDeLinea.java
@@ -0,0 +1,22 @@
+package basicos.salida;
+
+/**
+ * 
+ * @author Jorge
+ * 
+ * Imprime mensajes sin utilizar saltos de linea.
+ *
+ */
+
+public class ImprimeSinSaltoDeLinea { /* clase principal */
+
+	public static void main(String[] args) { /* metodo principal */
+		
+		System.out.print("Hola "); /* imprime Hola */
+		System.out.print("Juan"); /* imprime Juan */
+		
+		/* salida: "Hola Juan" */
+		
+	}
+	
+}
