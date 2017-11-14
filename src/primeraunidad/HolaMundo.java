d73b9125f3f67594fc28f4fa677d41a9e525b08f
Jorge Martinez <faith92.koko@gmail.com>
2017-11-13 20:27:19 -0600

    agregar el hola mundo

---
+++ src/primeraunidad/HolaMundo.java
@@ -0,0 +1,23 @@
+package primeraunidad;
+
+/**
+ * 
+ * @author Jorge
+ * 
+ * Imprime un mensaje en la consola utilizando una instruccion de salida.
+ * 
+ * Las instrucciones de salida consisten en llevar hacia el exterior los valores
+ * o texto. Estos textos son mostrados en una consola (console).
+ *
+ */
+
+public class HolaMundo { /* la clase principal (public class) se debe de llamar igual que el archivo .java */
+
+	public static void main (String [] args) { /* definicion del metodo principal */
+		
+		/* esta instruccion imprime "Hola Mundo!." en consola */
+		System.out.println("Hola Mundo!.");
+		
+	}
+	
+}
